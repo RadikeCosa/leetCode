4cbf8d4cb8b3a9822d39d3edcb45fa7d4a718229
Ramiro Cosa <ramirocosa@hotmail.com>
2025-05-22 09:54:29 -0300

    fix: se agrega consigna en integerToRoman

--- src/integerToRoman.ts
+++ src/integerToRoman.ts
@@ -1,3 +1,17 @@
+/**
+ * Dada una cantidad entera entre 1 y 3999, convertirla a su equivalente en números romanos.
+ *
+ * Reglas:
+ * - Los símbolos romanos válidos son: I (1), V (5), X (10), L (50), C (100), D (500), M (1000)
+ * - Se usan formas sustractivas para los valores: 4 (IV), 9 (IX), 40 (XL), 90 (XC), 400 (CD), 900 (CM)
+ * - Los símbolos se ordenan de mayor a menor, y solo I, X, C y M pueden repetirse hasta 3 veces.
+ *
+ * Ejemplos:
+ * intToRoman(3) => "III"
+ * intToRoman(58) => "LVIII"
+ * intToRoman(1994) => "MCMXCIV"
+ */
+
 export function intToRoman(num: number): string {
   const valMap: { value: number; symbol: string }[] = [
     { value: 1000, symbol: "M" },
